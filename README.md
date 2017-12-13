908786a42efa6b6255092cd701249ca165aee4e5
Robot7 <dewald@robot7.co.za>
2017-12-13 09:21:53 +0200

    Inital setup of git repository

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # flow-scripts
 Basic scripts to generate daily network traffic reports using flow-tools
+
+##Goals
+
+ - Summarize and report NetFlow data to facilitate exception management
+ - Multiple report formats
+ - Support in resolving non-repudiation of billing
+ - Simple code with minimal dependencies, using standard Debian packages, e.g. flow-tools, bash, awk, pandoc, pcregrep, aha
---
+++ scripts/top10-uploaders.sh
@@ -0,0 +1,44 @@
+#!/bin/dash
+
+# Report the top 10 internet uploaders: yesterday and month-to-date
+
+SOURCE_GLOB='/home/sysop/flows/max/*/*/ft-v05.*'
+HOSTNAME=$(hostname -f)
+REPORTTIME=$(/bin/date '+%Y-%m-%d %H:%M:%S' -d "yesterday")
+YESTERDAY=$(/bin/date '+%Y-%m-%d' -d "yesterday")
+TODAY=$(/bin/date '+%Y-%m-%d')
+THISMONTH=$(/bin/date '+%Y-%m')
+FIRSTOFMTH="$THISMONTH-01"
+HEADER1="IP Address\t\tHostname\t\t\t\t\tMBytes"
+HEADER2="==============\t\t=======================================\t\t======"
+
+AWK_SCRIPT='
+    {if (($1 ~ /^192\.168\.11\..+/) && ($2 !~ /^192\.168\.11\..+/) && ($2 !~ /^192\.168\.1\..+/))
+        ip_arr[$1]+=$4
+    } 
+    END { PROCINFO["sorted_in"] = "@val_num_desc"; 
+    for(ip in ip_arr) {
+        host_str=ip" ---"; 
+        "getent hosts " ip | getline host_str; 
+        split(host_str, host_arr, " "); 
+        printf "%-16s\t%-40s\t%dM\n", ip, host_arr[2], (ip_arr[ip]/(1048576))
+    }
+}'
+
+echo "\t---=(Top 10 uploaders: yesterday [$YESTERDAY])=---\n"
+echo "$HEADER1"
+echo "$HEADER2"
+/usr/bin/flow-cat -t "$YESTERDAY" -T "$TODAY" $SOURCE_GLOB \
+    | /usr/bin/flow-stat -f10 -S3 \
+    | /usr/bin/awk "$AWK_SCRIPT" \
+    | /usr/bin/head -10
+
+echo "\n\n\t---=(Top 10 uploaders: month-to-date [$THISMONTH])=---\n"
+echo "$HEADER1"
+echo "$HEADER2"
+/usr/bin/flow-cat -t "$FIRSTOFMTH" -T "$TODAY" $SOURCE_GLOB \
+    | /usr/bin/flow-stat -f10 -S3 \
+    | /usr/bin/awk "$AWK_SCRIPT" \
+    | /usr/bin/head -10
+
+echo "\nReported on $HOSTNAME at $REPORTTIME"
